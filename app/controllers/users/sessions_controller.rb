1d456a786b33f9e8d955b004177ea530c934143d
MannaPriyanka <mannapriyanka04@gmail.com>
2024-04-09 12:52:47 +0530

    Add auth token in Sign In API

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  skip_before_action :verify_authenticity_token, on: [:destroy, :create]
+
   # before_action :configure_sign_in_params, only: [:create]
   respond_to :json
   # GET /resource/sign_in
@@ -21,10 +23,11 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def respond_with(current_user, _opts = {})
+    token = generate_jwt_token(current_user)
     render json: {
       status: {
         code: 200, message: 'Logged in successfully.',
-        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
+        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] , token: token }
       }
     }, status: :ok
   end
@@ -46,6 +49,11 @@ class Users::SessionsController < Devise::SessionsController
         message: "Couldn't find an active session."
       }, status: :unauthorized
     end
+  end
+
+  def generate_jwt_token(user)
+    payload = { user_id: user.id, exp: 24.hours.from_now.to_i }
+    JWT.encode(payload, Rails.application.credentials.devise_jwt_secret_key)
   end
 
   # If you have extra params to permit, append them to the sanitizer.
